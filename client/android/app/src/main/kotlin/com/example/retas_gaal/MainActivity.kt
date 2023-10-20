6e8c4e4ff389ba29c852d3f37984af0a5f926508
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-10-20 11:12:38 -0300

    feat: client

---
+++ client/android/app/src/main/kotlin/com/example/retas_gaal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.retas_gaal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ client/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:retas_gaal/core/app.dart';
+
+void main() {
+  runApp(const App());
+}
